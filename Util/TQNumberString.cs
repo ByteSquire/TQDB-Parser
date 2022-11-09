54a31f69c21394f0f2109c44b55b110c4248bdb0
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2022-11-09 18:01:59 +0100

    feat(Util): add helper methods for working with number strings

---
+++ Util/TQNumberString.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace TQDB_Parser.Extensions
+{
+    public static class TQNumberString
+    {
+        public static bool TryParseTQString(string? s, out int result)
+        {
+            result = 0;
+            if (s is null)
+                return false;
+
+            return int.TryParse(s, NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out result);
+        }
+
+        public static bool TryParseTQString(string? s, out bool result)
+        {
+            result = false;
+            if (s is null)
+                return false;
+            if (s != "0" || s != "1")
+                return false;
+
+            return int.TryParse(s, NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out var b) && b != 0;
+        }
+
+        public static bool TryParseTQString(string? s, out float result)
+        {
+            result = 0;
+            if (s is null)
+                return false;
+
+            return float.TryParse(s, NumberStyles.AllowLeadingSign | NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out result);
+        }
+    }
+}
---
+++ Util/UtilExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace TQDB_Parser.Extensions
+{
+    public static class UtilExtensions
+    {
+        public static string ToTQString(this float self, bool convertToInt = false)
+        {
+            if (convertToInt)
+                return ToTQString((int)self);
+            return self.ToString("F6", CultureInfo.InvariantCulture);
+        }
+
+        public static string ToTQString(this int self, bool convertToFloat = false)
+        {
+            if (convertToFloat)
+                return ToTQString((float)self);
+            return self.ToString("D", CultureInfo.InvariantCulture);
+        }
+    }
+}
